15ff28fb092d57512ae8bcb61588757f0b8e9de4
Abdul Mohiz <skylark7768@gmail.com>
2024-08-10 17:53:03 +0500

    classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,213 +1,261 @@
 export default function Home() {
-  let var1: string = "Abdul Mohiz";
-  let var2: boolean = true;
+  // let var1: string = "Abdul Mohiz";
+  // let var2: boolean = true;
 
-  let var3: string = "Hello";
+  // let var3: string = "Hello";
 
-  //Defining Types
-  interface User {
-    name: string;
-    id: 0;
-  }
-
-  const user: User = {
-    name: "Mohiz",
-    id: 0,
-  };
-  type MyBool = true | false;
-  type WindowStates = "open" | "close" | "minimized";
-  type postive = 1 | 2 | 3 | 4;
-  function deleteUser(user: User) {}
-
-  // console.log(user.id + user.name);
-  let userId: number = 123.1;
-  let userName: string = "Mohiz";
-  let isUser: boolean = true;
-
-  let hero;
-  function getHero() {
-    return "Mohiz";
-  }
-  hero = getHero();
-  //console.log(hero);
-
-  function addTwo(num: number) {
-    return num + 2;
-  }
-  function getUpper(val: string) {
-    return val.toUpperCase();
-  }
-  // console.log(getUpper("mohiz"));
-  // console.log(addTwo(2));
-
-  let loginUser = (username: string, password: string, isPaid: boolean) => {
-    console.log(username, password, isPaid);
-  };
-  //loginUser("Hey", "2", false);
-
-  // function getValue(myVal: number):  {
-  //   if (myVal < 5) {
-  //     return true;
-  //   }
-  //   return "200 OK";
+  // //Defining Types
+  // interface User {
+  //   name: string;
+  //   id: 0;
   // }
 
-  const getHello = (name: string): string => {
-    return " ";
-  };
+  // const user: User = {
+  //   name: "Mohiz",
+  //   id: 0,
+  // };
+  // type MyBool = true | false;
+  // type WindowStates = "open" | "close" | "minimized";
+  // type postive = 1 | 2 | 3 | 4;
+  // function deleteUser(user: User) {}
 
-  const heroes = ["Mohiz", "Ali", "Ahmed"];
-  //const heroes = [1, 2, 3];
+  // // console.log(user.id + user.name);
+  // let userId: number = 123.1;
+  // let userName: string = "Mohiz";
+  // let isUser: boolean = true;
 
-  heroes.map((hero): string => {
-    return `hero is ${hero}`;
-  });
+  // let hero;
+  // function getHero() {
+  //   return "Mohiz";
+  // }
+  // hero = getHero();
+  // //console.log(hero);
 
-  function consoleError(errmsg: string): void {
-    console.log(errmsg);
-  }
+  // function addTwo(num: number) {
+  //   return num + 2;
+  // }
+  // function getUpper(val: string) {
+  //   return val.toUpperCase();
+  // }
+  // // console.log(getUpper("mohiz"));
+  // // console.log(addTwo(2));
 
-  function getError(errmsg: string): never {
-    throw new Error(errmsg);
-  }
+  // let loginUser = (username: string, password: string, isPaid: boolean) => {
+  //   console.log(username, password, isPaid);
+  // };
+  // //loginUser("Hey", "2", false);
 
-  // Objects
+  // // function getValue(myVal: number):  {
+  // //   if (myVal < 5) {
+  // //     return true;
+  // //   }
+  // //   return "200 OK";
+  // // }
 
-  const User = {
-    name: "Mohiz",
-    email: "123@gmail.com",
-    age: 23,
-  };
+  // const getHello = (name: string): string => {
+  //   return " ";
+  // };
 
-  function createUser({
-    name,
-    email,
-    age,
-  }: {
+  // const heroes = ["Mohiz", "Ali", "Ahmed"];
+  // //const heroes = [1, 2, 3];
+
+  // heroes.map((hero): string => {
+  //   return `hero is ${hero}`;
+  // });
+
+  // function consoleError(errmsg: string): void {
+  //   console.log(errmsg);
+  // }
+
+  // function getError(errmsg: string): never {
+  //   throw new Error(errmsg);
+  // }
+
+  // // Objects
+
+  // const User = {
+  //   name: "Mohiz",
+  //   email: "123@gmail.com",
+  //   age: 23,
+  // };
+
+  // function createUser({
+  //   name,
+  //   email,
+  //   age,
+  // }: {
+  //   name: string;
+  //   email: string;
+  //   age: number;
+  // }): void {
+  //   console.log(`Name: ${name}, Email: ${email}, Age: ${age}`);
+  // }
+  // //createUser(User);
+
+  // function check({
+  //   name = "Hira",
+  //   age = 13,
+  // }: {
+  //   name: string;
+  //   age: number;
+  // }): void {
+  //   console.log(name, age);
+  // }
+  // //check({});
+
+  // // Type Alias
+  // type Student = {
+  //   name: string;
+  //   age: number;
+  // };
+
+  // function createStudent(student: Student) {
+  //   return { name: student.name, age: student.age };
+  // }
+
+  // createStudent({ name: "Skylark", age: 23 });
+
+  // //Readonly
+  // type Person = {
+  //   readonly _id: string;
+  //   name: string;
+  //   age: number;
+  //   card?: number; //optional
+  // };
+
+  // let person: Person = {
+  //   _id: "123",
+  //   name: "Mohiz",
+  //   age: 23,
+  //   // card: 123,
+  // };
+  // // person._id='2'
+  // //console.log(person.card);
+
+  // type cardName = {
+  //   name: string;
+  // };
+
+  // type CardNumber = {
+  //   number: number;
+  // };
+  // type cardDetails = cardName &
+  //   CardNumber & {
+  //     cvc: number;
+  //   };
+
+  // let card: cardDetails = {
+  //   name: "Mohiz",
+  //   number: 123,
+  //   cvc: 123,
+  // };
+
+  // //Arrays
+  // const arr: string[] = ["Mohiz", "Ali", "Ahmed"];
+  // const arr2: number[] = [1, 2, 3];
+  // const arr3: Array<number> = [4, 5, 6]; //different way to define array with same logic
+
+  // // arr.push('Hira');
+  // // arr2.push(4);
+  // // console.log(arr);
+  // // console.log(arr2);
+
+  // //Union Types
+  // let val: string | number = 1;
+
+  // function getDbId(id: string | number) {
+  //   if (typeof id === "string") {
+  //     return id.toUpperCase();
+  //   }
+  //   return id;
+  // }
+
+  // //Type Guards
+  // function isString(val: string | number): val is string {
+  //   return typeof val === "string";
+  // }
+
+  // //Tuples
+  // type Tuple = [string, number];
+  // let tuple: Tuple = ["Mohiz", 123];
+  // let rgb: [number, number, number] = [255, 0, 0];
+  // rgb.push(123);
+  // // console.log(rgb)
+  // // console.log(tuple);
+
+  // //Enums
+  // enum Direction {
+  //   Up = 10,
+  //   Down = 15,
+  //   Left = "Mohiz",
+  //   Right = 2,
+  //   Middle,
+  // }
+  // let direction: Direction = Direction.Left;
+  // // console.log(direction);
+
+  // //Interface
+  // interface Employee {
+  //   name: string;
+  //   age: number;
+  //   role: string;
+  // }
+  // interface Manager extends Employee {
+  //   salary: number;
+  // }
+
+  //Classes
+
+  class User {
     name: string;
-    email: string;
     age: number;
-  }): void {
-    console.log(`Name: ${name}, Email: ${email}, Age: ${age}`);
-  }
-  //createUser(User);
-
-  function check({
-    name = "Hira",
-    age = 13,
-  }: {
-    name: string;
-    age: number;
-  }): void {
-    console.log(name, age);
-  }
-  //check({});
-
-  // Type Alias
-  type Student = {
-    name: string;
-    age: number;
-  };
-
-  function createStudent(student: Student) {
-    return { name: student.name, age: student.age };
-  }
-
-  createStudent({ name: "Skylark", age: 23 });
-
-  //Readonly
-  type Person = {
-    readonly _id: string;
-    name: string;
-    age: number;
-    card?: number; //optional
-  };
-
-  let person: Person = {
-    _id: "123",
-    name: "Mohiz",
-    age: 23,
-    // card: 123,
-  };
-  // person._id='2'
-  //console.log(person.card);
-
-  type cardName = {
-    name: string;
-  };
-
-  type CardNumber = {
-    number: number;
-  };
-  type cardDetails = cardName &
-    CardNumber & {
-      cvc: number;
-    };
-
-  let card: cardDetails = {
-    name: "Mohiz",
-    number: 123,
-    cvc: 123,
-  };
-
-  //Arrays
-  const arr: string[] = ["Mohiz", "Ali", "Ahmed"];
-  const arr2: number[] = [1, 2, 3];
-  const arr3: Array<number> = [4, 5, 6]; //different way to define array with same logic
-
-  // arr.push('Hira');
-  // arr2.push(4);
-  // console.log(arr);
-  // console.log(arr2);
-
-  //Union Types
-  let val: string | number = 1;
-
-  function getDbId(id: string | number) {
-    if (typeof id === "string") {
-      return id.toUpperCase();
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
     }
-    return id;
+    getDetails(): string {
+      return `Name: ${this.name}, Age: ${this.age}`;
+    }
   }
 
-  //Type Guards
-  function isString(val: string | number): val is string {
-    return typeof val === "string";
-  }
+  class Car {
+    private name: string;
+    model: string;
+    price: number;
 
-  //Tuples
-  type Tuple = [string, number];
-  let tuple: Tuple = ["Mohiz", 123];
-  let rgb: [number, number, number] = [255, 0, 0];
-  rgb.push(123);
-  // console.log(rgb)
-  // console.log(tuple);
+    constructor(n: string, m: string, p: number) {
+      this.name = n;
+      this.model = m;
+      this.price = p;
+    }
 
-  //Enums
-  enum Direction {
-    Up = 10,
-    Down = 15,
-    Left = "Mohiz",
-    Right = 2,
-    Middle,
+    getCarData(): string {
+      return `Name: ${this.name}, Model: ${this.model}, Price: ${this.price}`;
+    }
   }
-  let direction: Direction = Direction.Left;
-  // console.log(direction);
+  let car1 = new Car("Toyota", "Corolla", 2000000);
 
-  //Interface
-  interface Employee {
-    name: string;
-    age: number;
-    role: string;
+  //car1.name;
+  // console.log(car1.getCarData());
+
+  class Truck {
+    constructor(
+      public name: string,
+      public model: string,
+      public price: number
+    ) {}
+
+    getTruckData(): string {
+      return `Name: ${this.name}, Model: ${this.model}, Price: ${this.price}`;
+    }
   }
-  interface Manager extends Employee {
-    salary: number;
-  }
+  let truck1 = new Truck("Nissan", "8 Wheel", 50500300);
+  console.log(truck1.getTruckData());
 
   return (
     <main className="">
-      <h1>{var1}</h1>
-      <h1>{var2}</h1>
+      <h1></h1>
+      <h1></h1>
     </main>
   );
 }
